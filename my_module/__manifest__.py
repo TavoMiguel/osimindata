255274dd5b02519232c94ba9d674c3da0fcde3c1
TavoMiguel <gdonayre@gmail.com>
2018-11-03 02:11:12 -0500

    initial

---
+++ my_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+'name': "My Module",
+'summary': "Short subtitle phrase",
+'description': """Long description""",
+'author': "Your name",
+'license': "AGPL-3",
+'website': "http://www.example.com",
+'category': 'Uncategorized',
+'version': '11.0.1.0.0',
+'depends': ['base'],
+# 'data': ['views.xml'],
+# 'demo': ['demo.xml'],
+}
